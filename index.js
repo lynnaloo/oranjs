60459b9d06e6d0bd67d9afce00186eb256c223b8
Brian Anderson <banderson@dandb.com>
2016-10-05 11:45:59 -0700

    change the body background to orange

--- index.js
+++ index.js
@@ -2,3 +2,7 @@
 var now = new Date();
 // the cat says
 console.log('Feed the kitties at ' + now);
+
+//change the background color of the body of the document
+document.body.style.backgroundColor = "orange";
+
