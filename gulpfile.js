0f8b85ba7f44e43b6abbcfc510a6561931cd4014
Akram Rameez <rameez2612@gmail.com>
2016-10-06 01:41:03 +0530

    run task to uglify and minify js

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+const gulp = require('gulp');
+const uglify = require('gulp-uglify');
+
+// Uglify Task
+// Minifies JavaScript files
+gulp.task('uglify', () => {
+  gulp.src('app/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('default', ['uglify']);
