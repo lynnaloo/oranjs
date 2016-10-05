471f326aede3e5948fccab3035d74675f8a5762a
Gargi Rajadhyaksha <grajadhyaksha@atlmbp195.local>
2016-10-05 17:46:37 -0400

    Fix merging issues

---
+++ app/routes/index.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+/*
+exports.cat = function(req, res){
+  res.render('cat');
+};
+*/
+module.exports = router;
