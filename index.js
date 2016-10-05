188d0fe2157c84af22ea04fe51dc0f42da7148a6
bryantheastronaut <bgilbraith12@gmail.com>
2016-10-05 14:07:38 -0700

    added option for orange background

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-var clc = require('cli-color');
-var orange = clc.xterm(202).bgXterm(236);
+const clc = require('cli-color');
+const orange = clc.xterm(202).bgXterm(234);
+const orangeBg = clc.xterm(234).bgXterm(202);
 
 //cat's are always hungry
-var now = new Date();
+let now = new Date();
+let past = new Date(Date.now() - 3000)
 
 // the cat says
-console.log(orange('Feed the kitties at ' + now));
+console.log(orange(`Feed the kitties at ${now}`));
+console.log(orangeBg(`But I just fed them at ${past}!`))
