ed6fb99ddb9cf4cf057c045cc87c98a2d48ef4e4
Simon Painter <sjpainter19_uk@yahoo.co.uk>
2016-10-06 08:20:12 +0100

    yet more formatting corrections

--- specs/reminder.spec.js
+++ specs/reminder.spec.js
@@ -1,19 +1,20 @@
+'use strict'
+
 const chai = require('chai');
 const chaiString = require('chai-string');
 chai.should();
 chai.use(chaiString);
 
 describe('the index file', () => {
-   
+
     let reminderModule;
     let checkWaterReminderMessage;
-    describe('when loaded', () => {  
+    describe('when loaded', () => {
         before(() => {
             reminderModule = require('../app/reminder');
             checkWaterReminderMessage = reminderModule.CheckWater();
-        });       
+        });
 
-        
         it('should write a message to the console reminding the user to feed the kitties', () => {
            checkWaterReminderMessage.should.equal('Check your pets water and food, make sure its all fine :)');
         });
